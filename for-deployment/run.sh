752a084918db2b85b0d617a99a689c6726ba4944
Zander Bolgar <lezed1@gmail.com>
2020-04-20 02:01:10 -0400

    back to unique port

--- for-deployment/run.sh
+++ for-deployment/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker-compose pull
-docker-compose up -d
+docker-compose up --detach --no-build --force-recreate --always-recreate-deps --remove-orphans
 cloudflared tunnel --hostname boggl.lezed1.com --url http://localhost:3000
