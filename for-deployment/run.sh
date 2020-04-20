b63c8d0895851255f7be316f5fb52eb2a854ab67
Zander Bolgar <lezed1@gmail.com>
2020-04-20 02:02:00 -0400

    fix port in run.sh

--- for-deployment/run.sh
+++ for-deployment/run.sh
@@ -2,4 +2,4 @@
 
 docker-compose pull
 docker-compose up --detach --no-build --force-recreate --always-recreate-deps --remove-orphans
-cloudflared tunnel --hostname boggl.lezed1.com --url http://localhost:3000
+cloudflared tunnel --hostname boggl.lezed1.com --url http://localhost:44359
