f8d2647c996e2f30630a71ae8a4bec702bd124d6
Zander Bolgar <lezed1@gmail.com>
2020-05-03 22:40:29 -0400

    Go back to xy grid

--- boggl/imports/ui/Board.tsx
+++ boggl/imports/ui/Board.tsx
@@ -13,47 +13,32 @@ export const Board = withResizeDetector(({ width, height }) => {
     return <div>Loading...</div>;
   }
 
-  // return (
-  //   <div className="board">
-  //     <div className="grid-container">
-  //       <div className="grid-y grid-padding-y">
-  //         {game.board.map((row, i) => (
-  //           <div className="cell grid-x grid-padding-x" key={i}>
-  //             {row.map((die, i) => (
-  //               <div className="cell auto" key={i}>
-  //                 <div className="die">
-  //                   <div className="flex-container align-center-middle">
-  //                     <div className="die-text text-center">{die}</div>
-  //                   </div>
-  //                 </div>
-  //               </div>
-  //             ))}
-  //           </div>
-  //         ))}
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
   const squareSize = _.min([width, height]);
 
   return (
-    <div className="flex-container align-center-middle">
-      <div className="board" style={{ width: squareSize, height: squareSize }}>
-        {game.board.map((row) =>
-          row.map((die, i) => (
-            <svg className="die" key={i} viewBox="0 0 20 20">
-              <text
-                x="50%"
-                y="50%"
-                text-anchor="middle"
-                alignmentBaseline="central"
-              >
-                {die}
-              </text>
-            </svg>
-          ))
-        )}
+    <div
+      className="grid-container"
+      style={{ width: squareSize, height: squareSize }}
+    >
+      <div className="grid-y">
+        {game.board.map((row, i) => (
+          <div className="cell grid-x" key={i}>
+            {row.map((die, i) => (
+              <div className="die-container cell auto" key={i}>
+                <svg className="die" viewBox="0 0 20 20">
+                  <text
+                    x="50%"
+                    y="50%"
+                    text-anchor="middle"
+                    alignmentBaseline="central"
+                  >
+                    {die}
+                  </text>
+                </svg>{' '}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
